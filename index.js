5df9f3c040a7c53bf9b8c865f7a0aafe40048123
Rasel <raselranar2005@gmail.com>
2026-06-19 21:17:51 +0600

    add Express server setup and MongoDB connection with properties API endpoint

---
+++ index.js
@@ -0,0 +1,45 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+    const database = client.db("haven-stay");
+    const propertiesCollection = database.collection("properties");
+
+    //  fetch all properties
+    app.get("/api/properties", async (req, res) => {
+      const properties = await propertiesCollection.find().toArray();
+      res.send(properties);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
